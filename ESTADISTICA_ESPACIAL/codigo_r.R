5795a82de1737f83558403bcfb450ebc8786d96d
Gerardo Chaparro <32598591+LuisGerardo311@users.noreply.github.com>
2017-10-19 21:18:55 -0500

    Add files via upload

---
+++ ESTADISTICA_ESPACIAL/codigo_r.R
@@ -0,0 +1,22 @@
+library (maptools)
+getinfo.shape("D:/ESTADISTICA_ESPACIAL/sids2/sids2.shp")
+sids<-readShapePoly("D:/ESTADISTICA_ESPACIAL/sids2/sids2.shp")
+class(sids)
+
+
+library(rgdal)
+sids<- readOGR(dsn="D:/ESTADISTICA_ESPACIAL/sids2", layer="sids2")
+class(sids)
+  
+
+proj4string(sids)<-CRS("+proj=longlat +ellps=WGS84")
+plot(sids)
+
+
+library(spdep)
+sids_nbq<-poly2nb(sids)
+coords<-coordinates(sids)
+plot(sids)
+plot(sids_nbq, coords, add=T)
+
+coords<-coordinates(sids_SP)
